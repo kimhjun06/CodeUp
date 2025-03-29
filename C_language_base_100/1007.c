54107dcd8355621e42756909bfaad04d96366a2a
kimhjun06 <kimhjun06@gmail.com>
2025-03-29 17:52:50 +0900

    Update 1007.c

--- C_language_base_100/1007.c
+++ C_language_base_100/1007.c
@@ -1,3 +1,10 @@
+/*
+  윈도우 운영체제의 파일 경로를 출력하는 연습을 해보자.
+  파일 경로에는 특수문자들이 포함된다.
+  printf() 함수로 백슬래시 문자 \를 출력하기 위해서는
+  \\로 출력해야 한다.
+*/
+
 #include <stdio.h>
 int main(int argc, char** argv) {
   printf("\"C:\\Download\\hello.cpp\"");
