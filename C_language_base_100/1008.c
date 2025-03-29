89358fc23f677803dc16f5f0589680335e596ec0
kimhjun06 <kimhjun06@gmail.com>
2025-03-29 17:56:56 +0900

    Update 1008.c

--- C_language_base_100/1008.c
+++ C_language_base_100/1008.c
@@ -1,3 +1,9 @@
+/*
+    유니코드 특수문자를 출력하는 연습을 해보자.
+    CLI 명령어 입출력 화면에서 표나 그림을 그리기
+    위해 사용되던 방법과 비슷하다.
+*/
+
 #include <stdio.h>
 int main(int argc, char** argv) {
     printf("\u250C\u252C\u2510\n");
@@ -5,3 +11,4 @@ int main(int argc, char** argv) {
     printf("\u2514\u2534\u2518\n");
     return 0;
 }
+
