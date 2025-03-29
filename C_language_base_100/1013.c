84b426ae0d23daf1531ca15829c54c86216462d7
kimhjun06 <kimhjun06@gmail.com>
2025-03-29 23:38:45 +0900

    Create 1013.c

---
+++ C_language_base_100/1013.c
@@ -0,0 +1,11 @@
+/*
+  정수(int) 2개를 입력받아 그대로 출력해보자.
+*/
+
+#include <stdio.h>
+int main(int argc, char++ argv) {
+  int a = 0; b = 0;
+  scanf("%d %d", &a, &b);
+  printf("%d %d", a, b);
+  return 0;
+}
